b5d7e65914a0bbf8e57f1d82b97f91d718a4eb15
sec-xyz <87598270+sec-xyz@users.noreply.github.com>
2022-08-18 00:25:42 +0100

    Delete crAPI.sh

--- crAPI.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -o docker-compose.yml https://raw.githubusercontent.com/OWASP/crAPI/main/deploy/docker/docker-compose.yml
-docker-compose pull
-docker-compose -f docker-compose.yml --compatibility up -d
-
