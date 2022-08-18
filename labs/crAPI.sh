234a55bd936f441af48e408e4a12faa2b7c763c8
sec-xyz <87598270+sec-xyz@users.noreply.github.com>
2022-08-18 23:32:24 +0100

    Update crAPI.sh

--- labs/crAPI.sh
+++ labs/crAPI.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+if ! test -d crAPI
+then
+        mkdir crAPI && cd crAPI
+else
+        cd crAPI
+fi
+
 curl -o docker-compose.yml https://raw.githubusercontent.com/OWASP/crAPI/main/deploy/docker/docker-compose.yml
 docker-compose pull
 docker-compose -f docker-compose.yml --compatibility up -d
+
 
 
